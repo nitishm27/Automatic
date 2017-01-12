a8d53425db4e7801bdfd9fb543885e54ed04ae6e
Jonsm <sanmijon@mac.com>
2017-01-12 16:27:04 -0500

    tests for demodulation

--- scripts/fake_data_demod.py
+++ scripts/fake_data_demod.py
@@ -3,13 +3,15 @@ import ats.atsapi as atsapi
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+import acquisition.util
 
 npt = NPT(atsapi.Board(systemId=1, boardId=1))
-npt.post_trigger_samples = 1024
+npt.post_trigger_samples = 32
 npt.records_per_buffer = 100
-npt.samples_per_sec = 1e9
+npt.samples_per_sec = .25e9
 if_freq = 5e7
 rel_phase = np.pi / 2
+noiselevel = .02
 
 def gen_fake_data(npt, if_freq, rel_phase):
     ch1 = np.zeros(npt.post_trigger_samples * npt.records_per_buffer)
@@ -24,34 +26,41 @@ def gen_fake_data(npt, if_freq, rel_phase):
 
 def fit_demod(ch1, ch2, npt, if_freq):
     period = int(np.round(npt.samples_per_sec / if_freq))
-    xdata = np.linspace(0,period-1,period)
+    xdata = np.linspace(0,npt.post_trigger_samples-1,npt.post_trigger_samples)
     def wave(t, a, phase):
         return a*np.sin(2*np.pi * t / period + phase)
     mag = np.zeros(npt.records_per_buffer)
     phase = np.zeros(npt.records_per_buffer)
     for i in range(npt.records_per_buffer):
-        num_periods = int(npt.post_trigger_samples / period)
-        record_mag = np.zeros(num_periods)
-        record_phase = np.zeros(num_periods)
-        for j in range(num_periods):
-            ch1_period = ch1[i*npt.post_trigger_samples + j*period:i*npt.post_trigger_samples+(j+1)*period]
-            ch2_period = ch2[i*npt.post_trigger_samples + j*period:i*npt.post_trigger_samples+(j+1)*period]
-            ch1_opt = fit_single_period(xdata, wave, ch1_period, npt)
-            ch2_opt = fit_single_period(xdata, wave, ch2_period, npt)
-            record_mag[j] = ch1_opt[0]
-            record_phase[j] = ch1_opt[1] - ch2_opt[1]
-        mag[i] = np.mean(record_mag)
-        phase[i] = np.mean(np.unwrap(record_phase))
+        ch1_opt = fit_single(xdata, wave, ch1[i*npt.post_trigger_samples:(i+1)*npt.post_trigger_samples], npt)
+        ch2_opt = fit_single(xdata, wave, ch2[i*npt.post_trigger_samples:(i+1)*npt.post_trigger_samples], npt)
+        mag[i]=ch1_opt[0]
+        phase[i]=ch1_opt[1]-ch2_opt[1]
     return mag, np.unwrap(phase)
 
-def fit_single_period(xdata, wave, ydata, npt):
+def fit_single(xdata, wave, ydata, npt):
     peak = max(ydata)
     guess = [peak, np.arcsin(ydata[0]/peak)]
-    return curve_fit(wave, xdata, ydata, guess)[0]
+    return curve_fit(wave, xdata, ydata, guess, maxfev=2000)[0]
 
-ch1, ch2 = gen_fake_data(npt, if_freq, rel_phase)
-mag, phase = fit_demod(ch1, ch2, npt, if_freq)
+min_samples = 4
+max_samples = 40
+sample_time = 1e-6
 
-plt.plot(mag)
-plt.plot(phase)
+perror_old = []
+perror_fit = []
+for i in range(min_samples, max_samples):
+    npt.samples_per_sec = i * if_freq
+    npt.post_trigger_samples = int(sample_time * npt.samples_per_sec)
+    print(npt.post_trigger_samples * if_freq / npt.samples_per_sec)
+    ch1, ch2 = gen_fake_data(npt, if_freq, rel_phase)
+    ch1 += np.random.normal(0,noiselevel,len(ch1))
+    ch2 += np.random.normal(0,noiselevel,len(ch2))
+    phase = fit_demod(ch1, ch2, npt, if_freq)[1]
+    phase_old = acquisition.util.iq_demod_subtract(ch1, ch2, npt, if_freq)[1]
+    perror_fit.append(np.std(phase))
+    perror_old.append(np.std(phase_old))
+
+plt.plot(perror_fit)
+plt.plot(perror_old)
 plt.show()
